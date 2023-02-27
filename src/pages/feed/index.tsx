b532d1aff219e6c50169bb9447a346b7d1a9dd3e
Gabriel Araujo <gabriiel66@gmail.com>
2023-02-27 16:29:18 -0300

    feed page

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,23 +1,49 @@
-import type { NextPage } from 'next'
+import { prisma } from 'prisma/db'
+import { BasicTemplate, Card } from 'src/components'
 
-import { useAtom, sessionAtom } from 'src/atoms'
-import { BasicTemplate } from 'src/components'
-
-const FeedPage: NextPage = () => {
-  const [session, setSession] = useAtom(sessionAtom)
+const FeedPage = ({ postsData }) => {
+  const posts = JSON.parse(postsData) || {}
 
   return (
     <BasicTemplate>
       <h1
-        className="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter mt-14"
+        className="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter mb-14"
         data-aos="zoom-y-out"
       >
         <span className="bg-clip-text text-transparent gradient pl-2 pr-2">
           Feed
         </span>
       </h1>
+
+      <div className="w-full columns-1 md:columns-2 lg:columns-3">
+        {posts.map((post) => {
+          return (
+            <Card post={post} user={post.user} key={post.id} className="mb-4" />
+          )
+        })}
+      </div>
     </BasicTemplate>
   )
+}
+
+export async function getServerSideProps(context) {
+  const posts = await prisma.post.findMany({
+    where: {
+      deletedAt: null,
+    },
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return {
+    props: {
+      postsData: JSON.stringify(posts),
+    },
+  }
 }
 
 export default FeedPage
